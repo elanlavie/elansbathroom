8dd83146f06c6e31d53055cfd05f6bd80f494471
elanlavie <68617529+elanlavie@users.noreply.github.com>
2025-06-17 21:59:07 -0700

    Update app.py debug=False (from True)

--- app.py
+++ app.py
@@ -132,4 +132,4 @@ def clear_queue():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
