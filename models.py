c67c25e1170d93fc683865c6b7490c719b3896c9
elanlavie <68617529+elanlavie@users.noreply.github.com>
2025-06-17 22:14:39 -0700

    Update models.py to fix time issue

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class QueueEntry(db.Model):
     """Model for bathroom queue entries"""
     id = db.Column(db.Integer, primary_key=True)
     student_name = db.Column(db.String(50), nullable=False)
-    joined_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    joined_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
     
     def __repr__(self):
         return f'<QueueEntry {self.student_name}>'
@@ -14,3 +14,6 @@ class QueueEntry(db.Model):
         """Get the position of this student in the queue (1-based)"""
         earlier_entries = QueueEntry.query.filter(QueueEntry.joined_at < self.joined_at).count()
         return earlier_entries + 1
+
+
+
